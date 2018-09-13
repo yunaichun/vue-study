2e443730ff84dd6f285c73b9652b702bb03da43a
余乃春 <yunaichun@hikvision.com.cn>
2018-09-13 16:51:27 +0800

    Vue响应系统-nextTick异步更新队列

--- vue-src/core/observer/scheduler.js
+++ vue-src/core/observer/scheduler.js
@@ -6,19 +6,22 @@ import { callHook, activateChildComponent } from '../instance/lifecycle'
 
 import {
   warn,
-  nextTick,
+  nextTick,   
   devtools
 } from '../util/index'
 
 export const MAX_UPDATE_COUNT = 100
 
+// queue 常量是一个数组，存储异步观察者回调队列的
 const queue: Array<Watcher> = []
 const activatedChildren: Array<Component> = []
-// 一个哈希表，用来存放watcher对象的id，防止重复的watcher对象多次加入
+// has 的作用就是用来避免将相同的观察者重复入队的
 let has: { [key: number]: ?true } = {}
 let circular: { [key: number]: number } = {}
-let waiting = false
+// flushing 变量是一个标志，放入队列 queue 中的所有观察者将会在突变完成之后统一执行更新，当更新开始时会将 flushing 变量的值设置为 true，代表着此时正在执行更新
 let flushing = false
+// waiting 同样是一个标志，无论调用多少次 queueWatcher 函数，该 if 语句块的代码只会执行一次
+let waiting = false
 let index = 0
 
 /**
@@ -37,7 +40,7 @@ function resetSchedulerState () {
 /**
  * Flush both queues and run the watchers.
  */
-// nextTick的回调函数，在下一个tick时flush掉两个队列同时运行watchers
+// flushSchedulerQueue 函数的作用之一就是用来将队列中的观察者统一执行更新的
 function flushSchedulerQueue () {
   flushing = true
   let watcher, id
@@ -142,7 +145,7 @@ function callUpdatedHooks (queue) {
  * The queue will be processed after the entire tree has been patched.
  */
 /*
-  在patch期间被激活（activated）的keep-alive组件保存在队列中，
+     -alive组件保存在队列中，
   是到patch结束以后该队列会被处理
  */
 export function queueActivatedComponent (vm: Component) {
@@ -157,18 +160,33 @@ export function queueActivatedComponent (vm: Component) {
  * Jobs with duplicate IDs will be skipped unless it's
  * pushed when the queue is being flushed.
  */
-// 将一个观察者对象push进观察者队列，在队列中已经存在相同的id则该观察者对象将被跳过
-// 除非它是在队列被刷新时推送
+// 将观察者放到一个队列中等待所有突变完成之后统一执行更新
 export function queueWatcher (watcher: Watcher) {
+  // 察者对象的唯一 id
   const id = watcher.id
+  // has 的作用就是用来避免将相同的观察者重复入队的
   if (has[id] == null) {
+    // 将该观察者的 id 值登记到 has 对象上作为 has 对象的属性同时将该属性值设置为 true
     has[id] = true
+    /*
+      flushing 变量是一个标志，我们知道放入队列 queue 中的所有观察者将会在突变完成之后统一执行更新，
+      当更新开始时会将 flushing 变量的值设置为 true，代表着此时正在执行更新，
+      所以根据判断条件 if (!flushing) 可知只有当队列没有执行更新时才会简单地将观察者追加到队列的尾部，
+
+      有的同学可能会问：“难道在队列执行更新的过程中还会有观察者入队的操作吗？”，实际上是会的，
+      典型的例子就是计算属性，比如队列执行更新时经常会执行渲染函数观察者的更新，渲染函数中很可能有计算属性的存在，
+      由于计算属性在实现方式上与普通响应式属性有所不同，所以当触发计算属性的 get 拦截器函数时会有观察者入队的行为，这个时候我们需要特殊处理，也就是 else 分支的代码
+    */
     if (!flushing) {
       // 如果没有flush掉，直接push到队列中即可
       queue.push(watcher)
     } else {
       // if already flushing, splice the watcher based on its id
       // if already past its id, it will be run next immediately.
+      /*
+        当变量 flushing 为真时，说明队列正在执行更新，这时如果有观察者入队则会执行 else 分支中的代码，
+        这段代码的作用是为了保证观察者的执行顺序
+      */
       let i = queue.length - 1
       while (i > index && queue[i].id > watcher.id) {
         i--
@@ -178,6 +196,7 @@ export function queueWatcher (watcher: Watcher) {
     // queue the flush
     if (!waiting) {
       waiting = true
+      // flushSchedulerQueue 函数的作用之一就是用来将队列中的观察者统一执行更新的
       nextTick(flushSchedulerQueue)
     }
   }
--- vue-src/core/util/env.js
+++ vue-src/core/util/env.js
@@ -79,21 +79,92 @@ export const hasSymbol =
     目的是延迟到当前调用栈执行完以后执行
 */
 export const nextTick = (function () {
-  // 存放异步执行的回调
-  const callbacks = []
-  // 一个标记位，如果已经有timerFunc被推送到任务队列中去则不需要重复推送
+  /*
+    它是一个标识，它的真假代表回调队列是否处于等待刷新的状态，初始值是 false 代表回调队列为空不需要等待刷新。
+    假如此时在某个地方调用了 $nextTick 方法，那么 if 语句块内的代码将会被执行，
+    在 if 语句块内优先将变量 pending 的值设置为 true，代表着此时回调队列不为空，正在等待刷新。
+  */
   let pending = false
-  // 一个函数指针，指向函数将被推送到任务队列中，等到主线程任务执行完时，任务队列中的timerFunc被调用
+  // 相对主线程来说，这是一个异步任务队列的统一存放数组【callbacks -> nextTickHandler -> timerFunc】
+  const callbacks = []
+  // 相对主线程来说，这是一个异步任务队列的统一执行函数【callbacks -> nextTickHandler -> timerFunc】
   let timerFunc
 
-  // 下一个tick时的回调：执行callbacks中存储的函数
+  // 相对主线程来说，这是一个异步任务队列的统一存放数组的包裹回调函数【callbacks -> nextTickHandler -> timerFunc】
   function nextTickHandler () {
-    // 一个标记位，标记等待状态（即函数已经被推入任务队列或者主线程，已经在等待当前栈执行完毕去执行）
-    // 这样就不需要在push多个回调到callbacks时将timerFunc多次推入任务队列或者主线程
+    // 首先将变量 pending 重置为 false
     pending = false
-    // 执行所有callback
+    // 接着开始执行回调，但需要注意的是在执行 callbacks 队列中的回调函数时并没有直接遍历 callbacks 数组，而是使用 copies 常量保存一份 callbacks 的复制，
     const copies = callbacks.slice(0)
+    /*   
+      遍历 copies 数组之前将 callbacks 数组清空：callbacks.length = 0
+      为什么要这么做呢？这么做肯定是有原因的，我们模拟一下整个异步更新的流程就明白了，如下代码：
+        created () {
+          this.name = 'HcySunYang'
+          this.$nextTick(() => {
+            this.name = 'hcy'
+            this.$nextTick(() => { console.log('第二个 $nextTick') })
+          })
+        }
+      上面代码中我们在外层 $nextTick 方法的回调函数中再次调用了 $nextTick 方法，
+      理论上外层 $nextTick 方法的回调函数不应该与内层 $nextTick 方法的回调函数在同一个 microtask 任务中被执行，
+      而是两个不同的 microtask 任务，虽然在结果上看或许没什么差别，但从设计角度就应该这么做。
+
+
+      我们注意上面代码中我们修改了两次 name 属性的值(假设它是响应式数据)，首先我们将 name 属性的值修改为字符串 HcySunYang，
+      我们前面讲过这会导致依赖于 name 属性的渲染函数观察者被添加到 queue 队列中，
+      这个过程是通过调用 src/core/observer/scheduler.js 文件中的 queueWatcher 函数完成的。
+      同时在 queueWatcher 函数内会使用 nextTick 将 flushSchedulerQueue 添加到 callbacks 数组中，所以此时 callbacks 数组如下：
+      callbacks = [
+        flushSchedulerQueue // queue = [renderWatcher]
+      ]
+
+
+      同时会将 nextTickHandler 函数注册为 microtask，所以此时 microtask 队列如下：
+      // microtask 队列
+      [
+        nextTickHandler
+      ]
+
+
+      接着调用了第一个 $nextTick 方法，$nextTick 方法会将其回调函数添加到 callbacks 数组中，那么此时的 callbacks 数组如下：
+      callbacks = [
+        flushSchedulerQueue, // queue = [renderWatcher]
+        () => {
+          this.name = 'hcy'
+          this.$nextTick(() => { console.log('第二个 $nextTick') })
+        }
+      ]
+
+
+      接下来主线程处于空闲状态(调用栈清空)，开始执行 microtask 队列中的任务，即执行 nextTickHandler 函数，
+      nextTickHandler 函数会按照顺序执行 callbacks 数组中的函数，首先会执行 flushSchedulerQueue 函数，
+      这个函数会遍历 queue 中的所有观察者并重新求值，完成重新渲染(re-render)，
+      在完成渲染之后，本次更新队列已经清空，queue 会被重置为空数组，一切状态还原。接着会执行如下函数：
+      () => {
+        this.name = 'hcy'
+        this.$nextTick(() => { console.log('第二个 $nextTick') })
+      }
+
+
+      这个函数是第一个 $nextTick 方法的回调函数，由于在执行该回调函数之前已经完成了重新渲染，所以该回调函数内的代码是能够访问更新后的DOM的，到目前为止一切都很正常，
+      继续往下看，在该回调函数内再次修改了 name 属性的值为字符串 hcy，这会再次触发响应，同样的会调用 nextTick 函数将 flushSchedulerQueue 添加到 callbacks 数组中，
+      但是由于在执行 nextTickHandler 函数时优先将 pending 的重置为 false，
+      所以 nextTick 函数会将 nextTickHandler 函数注册为一个新的 microtask，此时 microtask 队列将包含两个 nextTickHandler 函数：
+      // microtask 队列
+      [
+        nextTickHandler, // 第一个 nextTickHandler
+        nextTickHandler  // 第二个 nextTickHandler
+      ]
+
+
+      怎么样？我们的目的达到了，现在有两个 microtask 任务。
+      而另外除了将变量 pending 的值重置为 false 之外，我们要知道第一个 nextTickHandler 函数遍历的并不是 callbacks 本身，
+      而是它的复制品 copies 数组，并且在第一个 nextTickHandler 函数的一开头就清空了 callbacks 数组本身。
+      所以第二个 nextTickHandler 函数的一切流程与第一个 nextTickHandler 是完全相同。
+    */
     callbacks.length = 0
+    // 然后遍历 copies 数组
     for (let i = 0; i < copies.length; i++) {
       copies[i]()
     }
@@ -109,14 +180,28 @@ export const nextTick = (function () {
   // same loop is by using MessageChannel.
   /* istanbul ignore if */
   /* 
-  https://www.jianshu.com/p/4f07ef18b5d7 
-  常见的 macro task 有 setTimeout、MessageChannel、postMessage、setImmediate
-  常见的 micro task 有 MutationObsever 和 Promise.then
+    https://www.jianshu.com/p/4f07ef18b5d7 
+    常见的 macro task 有 setTimeout、MessageChannel、postMessage、setImmediate
+    常见的 micro task 有 MutationObsever 和 Promise.then
+
+    我们知道任务队列并非只有一个队列，在 node 中更为复杂，但总的来说我们可以将其分为 microtask 和 (macro)task，
+    并且这两个队列的行为还要依据不同浏览器的具体实现去讨论，这里我们只讨论被广泛认同和接受的队列执行行为。
+    当调用栈空闲后每次事件循环只会从 (macro)task 中读取一个任务并执行，而在同一次事件循环内会将 microtask 队列中所有的任务全部执行完毕，且要先于 (macro)task。
+    另外 (macro)task 中两个不同的任务之间可能穿插着UI的重渲染，那么我们只需要在 microtask 中把所有在UI重渲染之前需要更新的数据全部更新，这样只需要一次重渲染就能得到最新的DOM了。
+    恰好 Vue 是一个数据驱动的框架，如果能在UI重渲染之前更新所有数据状态，这对性能的提升是一个很大的帮助，所有要优先选用 microtask 去更新数据状态而不是 (macro)task，
+    这就是为什么不使用 setTimeout 的原因，因为 setTimeout 会将回调放到 (macro)task 队列中而不是 microtask 队列，
+    所以理论上最优的选择是使用 Promise，当浏览器不支持 Promise 时再降级为 setTimeout
   */
-  // 优先级：setImmediate -> MessageChannel -> Promise.then -> setTimeout
+  // 定义timerFunc函数 -> 优先级：setImmediate -> MessageChannel -> Promise.then -> setTimeout
   if (typeof setImmediate !== 'undefined' && isNative(setImmediate)) {
     timerFunc = () => {
-      // 执行callbacks中存储的函数
+      /*
+        为什么首选 setImmediate 呢？这是有原因的，因为 setImmediate 拥有比 setTimeout 更好的性能，
+        这个问题很好理解，setTimeout 在将回调注册为 (macro)task 之前要不停的做超时检测，而 setImmediate 则不需要，这就是优先选用 setImmediate 的原因。
+
+        但是 setImmediate 的缺陷也很明显，就是它的兼容性问题，到目前为止只有IE浏览器实现了它，
+        所以为了兼容非IE浏览器我们还需要做兼容处理，只不过此时还轮不到 setTimeout 上场，而是使用 MessageChannel
+      */
       setImmediate(nextTickHandler)
     }
   } 
@@ -127,6 +212,12 @@ export const nextTick = (function () {
       MessageChannel.toString() === '[object MessageChannelConstructor]'
     )
   ) {
+    /*
+      Web Workers 的内部实现就是用到了 MessageChannel，一个 MessageChannel 实例对象拥有两个属性 port1 和 port2，
+      我们只需要让其中一个 port 监听 onmessage 事件，然后使用另外一个 port 的 postMessage 向前一个 port 发送消息即可，
+
+      这样前一个 port 的 onmessage 回调就会被注册为 (macro)task，由于它也不需要做任何检测工作，所以性能也要优于 setTimeout
+    */
     const channel = new MessageChannel()
     const port = channel.port2
     channel.port1.onmessage = nextTickHandler
@@ -150,12 +241,28 @@ export const nextTick = (function () {
     }
   }
 
-  // 推送到队列中下一个tick时执行，cb 回调函数，ctx 上下文
+  // cb 回调函数，ctx 上下文
   return function queueNextTick (cb?: Function, ctx?: Object) {
+    /*
+      当 nextTick 函数没有接收到 cb 参数时，会检测当前宿主环境是否支持 Promise，如果支持则直接返回一个 Promise 实例对象，
+      并且将 resolve 函数赋值给 _resolve 变量，_resolve 变量声明在 nextTick 函数的顶部。
+    */
     let _resolve
-    // 将cb任务推送到callbacks数组中 (存放异步执行的回调)
+    /*
+      注意并不是将 cb 回调函数直接添加到 callbacks 数组中，但这个被添加到 callbacks 数组中的函数的执行会间接调用 cb 回调函数，
+      并且可以看到在调用 cb 函数时使用 .call 方法将函数 cb 的作用域设置为 ctx，也就是 nextTick 函数的第二个参数。
+
+      所以对于 $nextTick 方法来讲，传递给 $nextTick 方法的回调函数的作用域就是当前组件实例对象，当然了前提是回调函数不能是箭头函数，
+      其实在平时的使用中，回调函数使用箭头函数也没关系，只要你能够达到你的目的即可。
+
+      另外我们再次强调一遍，此时回调函数并没有被执行，当你调用 $nextTick 方法并传递回调函数时，
+      会使用一个新的函数包裹回调函数并将新函数添加到 callbacks 数组中。
+    */
     callbacks.push(() => {
-      // nextTick传入有函数
+      /*
+        当 nextTickHandler 函数开始执行 callbacks 数组中的函数时，
+        如果有传递 cb 参数，则直接执行cb回调函数
+      */
       if (cb) {
         try {
           cb.call(ctx)
@@ -163,19 +270,34 @@ export const nextTick = (function () {
           handleError(e, ctx, 'nextTick')
         }
       }
+      /*
+        当 nextTickHandler 函数开始执行 callbacks 数组中的函数时，
+        如果没有传递 cb 参数，则直接调用 _resolve 函数，我们知道这个函数就是返回的 Promise 实例对象的 resolve 函数。
+        这样就实现了 Promise 方式的 $nextTick 方法。
+      */
       else if (_resolve) {
         _resolve(ctx)
       }
     })
     // 执行异步回调
     if (!pending) {
-      // 一个标记位，标记等待状态（即函数已经被推入任务队列或者主线程，已经在等待当前栈执行完毕去执行）
-      // 这样就不需要在push多个回调到callbacks时将timerFunc多次推入任务队列或者主线程
+      /*
+        它是一个标识，它的真假代表回调队列是否处于等待刷新的状态，初始值是 false 代表回调队列为空不需要等待刷新。
+        假如此时在某个地方调用了 $nextTick 方法，那么 if 语句块内的代码将会被执行，
+        在 if 语句块内优先将变量 pending 的值设置为 true，代表着此时回调队列不为空，正在等待刷新。
+      */
       pending = true
-      // callbacks存储在timerFunc的nextTickHandler函数中
+      /* 
+        相对主线程来说，这是一个异步任务队列的统一执行函数【callbacks -> nextTickHandler -> timerFunc】
+        执行timerFunc -> 执行nextTickHandler -> 执行callbacks中存放的cb回调函数
+      */
       timerFunc()
     }
     // $flow-disable-line
+    /*
+      当 nextTick 函数没有接收到 cb 参数时，会检测当前宿主环境是否支持 Promise，如果支持则直接返回一个 Promise 实例对象，
+      并且将 resolve 函数赋值给 _resolve 变量，_resolve 变量声明在 nextTick 函数的顶部。
+    */
     if (!cb && typeof Promise !== 'undefined') {
       return new Promise((resolve, reject) => {
         _resolve = resolve
