73b726751d3fcab8732647c3d47f868fe049b1ed
余乃春 <yunaichun@hikvision.com.cn>
2018-12-22 14:25:48 +0800

    路由映射表存在 redirect：redirect方法处理重定向

--- vue-router-src/create-matcher.js
+++ vue-router-src/create-matcher.js
@@ -21,7 +21,7 @@ export function createMatcher (
   /*一、根据 routes 配置对象创建路由 map：
     pathList：存储所有路由配置的 path
     pathMap：path <=> 路由记录 映射表
-    nameMap：name <=> 路由记录  映射表
+    nameMap：name <=> 路由记录 映射表
   */
   const { pathList, pathMap, nameMap } = createRouteMap(routes)
 
@@ -46,12 +46,16 @@ export function createMatcher (
     const location = normalizeLocation(raw, currentRoute, false, router)
     const { name } = location
 
+    /*一、路由 url.parse 对象中含有 name*/
     if (name) {
+      /*nameMap：name <=> 路由记录  映射表*/
       const record = nameMap[name]
+      /*此 name 的 路由记录  映射表 不存在，报错，创建空路由*/
       if (process.env.NODE_ENV !== 'production') {
         warn(record, `Route with name '${name}' does not exist`)
       }
       if (!record) return _createRoute(null, location)
+
       const paramNames = record.regex.keys
         .filter(key => !key.optional)
         .map(key => key.name)
@@ -72,7 +76,9 @@ export function createMatcher (
         location.path = fillParams(record.path, location.params, `named route "${name}"`)
         return _createRoute(record, location, redirectedFrom)
       }
-    } else if (location.path) {
+    }
+    /*二、路由 url.parse 对象中不含有 name，但是 path 存在*/
+    else if (location.path) {
       location.params = {}
       for (let i = 0; i < pathList.length; i++) {
         const path = pathList[i]
@@ -82,72 +88,119 @@ export function createMatcher (
         }
       }
     }
+
     // no match
+    /*三、路由 url.parse 对象中不含有 name，也不含有 path*/
     return _createRoute(null, location)
   }
 
-  function redirect (
-    record: RouteRecord,
-    location: Location
+  /*创建路由：分为两种情况*/
+  function _createRoute (
+    record: ?RouteRecord, /*路由映射表*/
+    location: Location, /*规范化处理 location：1、字符串 path 2、对象 query 3、字符串 hash*/
+    /*redirectedFrom 参数含义：与参数 2 类似
+      1、根据 base 获取浏览器 window 地址location：pathname + search + hash（对 HTML5History 来说）
+      2、获取浏览器 window 地址的 hash 值（对 HashHistory 来说） 
+      3、非浏览器环境获取 stack 数组最后一项的 fullPath（对 AbstractHistory 来说）
+    */
+    redirectedFrom?: Location 
   ): Route {
+    /*一、路由映射表存在 redirect：重定向*/
+    if (record && record.redirect) {
+      return redirect(record, redirectedFrom || location)
+    }
+    /*二、路由映射表存在 matchAs：重定向*/
+    if (record && record.matchAs) {
+      return alias(record, location, record.matchAs)
+    }
+    /*三、路由映射表不存在 redirect 和 matchAs，创建路由*/
+    return createRoute(record, location, redirectedFrom, router)
+  }
+
+  /*路由映射表存在 redirect：重定向*/
+  function redirect (
+    record: RouteRecord, /*路由映射表*/
+    location: Location /*规范化处理 location：1、字符串 path 2、对象 query 3、字符串 hash*/
+  ): Route {
+    /*路由映射表：中最初的 redirect 重定向地址*/
     const originalRedirect = record.redirect
     let redirect = typeof originalRedirect === 'function'
-      ? originalRedirect(createRoute(record, location, null, router))
+      ? originalRedirect(createRoute(record, location, null, router)) /*如果 originalRedirect 为 function 创建路由对象*/
       : originalRedirect
 
+    /*redirect 为 string 的话拼接为对象 */
     if (typeof redirect === 'string') {
       redirect = { path: redirect }
     }
 
+    /*redirect 不为对象的话报错*/
     if (!redirect || typeof redirect !== 'object') {
       if (process.env.NODE_ENV !== 'production') {
         warn(
           false, `invalid redirect option: ${JSON.stringify(redirect)}`
         )
       }
+      /*创建空路由*/
       return _createRoute(null, location)
     }
 
     const re: Object = redirect
     const { name, path } = re
     let { query, hash, params } = location
+    /*redirect 中 query、hash、params 处理*/
     query = re.hasOwnProperty('query') ? re.query : query
     hash = re.hasOwnProperty('hash') ? re.hash : hash
     params = re.hasOwnProperty('params') ? re.params : params
 
+    /*一、重定向路由 url.parse 对象中含有 name【即 name 改变了】*/
     if (name) {
       // resolved named direct
+      /*nameMap：name <=> 路由记录  映射表*/
       const targetRecord = nameMap[name]
       if (process.env.NODE_ENV !== 'production') {
+        /*此 name 的 路由记录  映射表 不存在，报错*/
         assert(targetRecord, `redirect failed: named route "${name}" not found.`)
       }
+      /*重定向：即重新调用 match 函数*/
       return match({
         _normalized: true,
-        name,
+        name, /*重定向的 name*/
         query,
         hash,
         params
       }, undefined, location)
-    } else if (path) {
+    }
+    /*二、重定向路由 url.parse 对象中不含有 name，但是 path 存在【即 path 改变了】*/
+    else if (path) {
       // 1. resolve relative redirect
+      /*当前 redirect 的 path 相对 路由映射表 record 的路径*/
       const rawPath = resolveRecordPath(path, record)
       // 2. resolve params
+      /*将动态路由解析成对象的形式：
+        例：let toPath = Regexp.compile('/user/:id')
+            toPath({ id: 123 }) //=> "/user/123"
+      */
       const resolvedPath = fillParams(rawPath, params, `redirect route with path "${rawPath}"`)
       // 3. rematch with existing query and hash
+      /*重定向：即重新调用 match 函数*/
       return match({
         _normalized: true,
-        path: resolvedPath,
+        path: resolvedPath, /*重定向的 path*/
         query,
         hash
       }, undefined, location)
-    } else {
+    }
+    /*三、重定向路由 url.parse 对象中不含有 name，也不含有 path【即 name 和 path 都没改变】*/
+    else {
       if (process.env.NODE_ENV !== 'production') {
         warn(false, `invalid redirect option: ${JSON.stringify(redirect)}`)
       }
+      /*创建空路由*/
       return _createRoute(null, location)
     }
   }
 
+  /*路由映射表存在 matchAs：重定向*/
   function alias (
     record: RouteRecord,
     location: Location,
@@ -165,20 +218,6 @@ export function createMatcher (
       return _createRoute(aliasedRecord, location)
     }
     return _createRoute(null, location)
-  }
-
-  function _createRoute (
-    record: ?RouteRecord,
-    location: Location,
-    redirectedFrom?: Location
-  ): Route {
-    if (record && record.redirect) {
-      return redirect(record, redirectedFrom || location)
-    }
-    if (record && record.matchAs) {
-      return alias(record, location, record.matchAs)
-    }
-    return createRoute(record, location, redirectedFrom, router)
   }
 
   /*最后：返回 matcher 对象*/
@@ -213,6 +252,13 @@ function matchRoute (
   return true
 }
 
+/*处理当前 redirect 的 path 相对 路由映射表 record 的路径*/
 function resolveRecordPath (path: string, record: RouteRecord): string {
+  /*相对路径、基础路径、相对路径是否拼接到基础路径之后
+    1、resolvePath('/aaa', '/bbb', false)   ->   "/aaa"
+    2、resolvePath('?aaa', '/bbb', false)   ->   "/bbb?aaa"
+    3、resolvePath('aaa', '/bbb', true)   ->   "/bbb/aaa"
+      resolvePath('aaa', '/bbb', false)   ->   "/aaa"
+  */
   return resolvePath(path, record.parent ? record.parent.path : '/', true)
 }
