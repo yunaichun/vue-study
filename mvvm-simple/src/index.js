79aa46aa69576daeaab3597477844bf1652deb5d
yunaichun <yunaichun@qingting.fm>
2022-09-13 17:14:39 +0800

    feat(NoTicket): update mvvm

--- mvvm-simple/src/index.js
+++ mvvm-simple/src/index.js
@@ -20,37 +20,43 @@ observer(data);
 /** 第三步: 定义观察者 Watcher。当数据 data 有变化时，执行相应的回调方法 */
 /** 1、检测对象 */
 new Watcher(data, 'a', function(newVal, oldVal) {
-  console.log('新值: ' + newVal + '----' + '旧值: ' + oldVal);
+  console.log('新值: ', newVal);
+  console.log('旧值: ', oldVal);
 });
 data.a = 300;
 console.log('data', data);
 
 /** 2、监测对象嵌套对象 */
 new Watcher(data, 'level1.level2.w', function(newVal, oldVal) {
-  console.log('新值: ' + newVal + '----' + '旧值: ' + oldVal);
+  console.log('新值: ', newVal);
+  console.log('旧值: ', oldVal);
 });
 data.level1.level2.w = 'modifystr';
 
 /** 3、监测对象嵌套数组 */
 new Watcher(data, 'level1.level2.x', function(newVal, oldVal) {
-  console.log('新值: ' + newVal + '----' + '旧值: ' + oldVal);
+  console.log('新值: ', newVal);
+  console.log('旧值: ', oldVal);
 });
 data.level1.level2.x.push(2);
  
 /** 4、监测数组 */
 new Watcher(data, 'level1.c', function(newVal, oldVal) {
-  console.log('新值: ' + newVal + '----' + '旧值: ' + oldVal);
+  console.log('新值: ', newVal);
+  console.log('旧值: ', oldVal);
 });
 data.level1.c.push(4);
 
 /** 5、监测数组嵌套对象 */
 new Watcher(data, 'level1.c.0.w', function(newVal, oldVal) {
-  console.log('新值: ' + newVal + '----' + '旧值: ' + oldVal);
+  console.log('新值: ', newVal);
+  console.log('旧值: ', oldVal);
 });
 data.level1.c[0].w = 100;
 
 /** 6、监测数组嵌套数组 */
 new Watcher(data, 'level1.c.1.x', function(newVal, oldVal) {
-  console.log('新值: ' + newVal + '----' + '旧值: ' + oldVal);
+  console.log('新值: ', newVal);
+  console.log('旧值: ', oldVal);
 });
 data.level1.c[1].x.push(4);
